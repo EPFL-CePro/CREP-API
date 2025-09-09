df5b89306ce75741916f210cbafad6337e0d8915
Azecko <samikarin24@gmail.com>
2025-09-09 12:47:10 +0200

    [feature] MySQL, `/exams` route
    There is now a route named `/exams` that queries the `exam` table and return everything in it as JSON format.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mysql = require('mysql2')
+process.loadEnvFile() // Required for Node to read the `.env` file, God knows why... Thanks to https://stackoverflow.com/a/79715113
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/exams', (req, res) => {
+    const connection = mysql.createConnection({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+    })
+    connection.connect()
+
+    connection.query('SELECT * from exam;', (err, rows, fields) => {
+        if (err) throw err
+
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(rows));
+    })
+
+    connection.end()
 })
 
 app.listen(port, () => {
